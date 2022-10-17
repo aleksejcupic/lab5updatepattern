5c9e5a06cfc629c0338968d5bc25d3abded0c699
Aleksej Cupic <aleksejcupic@gmail.com>
2022-10-17 12:30:58 -0400

    more work

--- script.js
+++ script.js
@@ -1,6 +1,5 @@
 // CHART INIT ------------------------------
-let type = d3.select().node() 
-document.querySelector() // access value property 
+document.querySelector('#group-by option:checked').value;
 
 var margin = {top: 20, right: 10, bottom: 20, left: 10};
 
@@ -13,52 +12,89 @@ const svg = d3.select(".chart").append("svg")
   .append("g")
     .attr("transform", "translate(" + margin.left + "," + margin.top + ")");
 
-// create scales without domains
-    // ordinal scale using scaleBand()
-    // data.map(d => d.company) to derive company names
-    // rangeRound() to avoid aliasing 
-    // paddingInner(0.1)
-const xScale = d3.scaleBand()
-var x = d3.scaleBand.scaleOrdinal()
-.range([0, width]);
-
-const yScale = d3.scale.linear()
-    .range([height, 0]);
-
-svg.append('g')
-    .attr('class', 'axis y-axis');
-svg.append('g')
-    .attr('class', 'axis x-axis')
-    // more
-    ;
-svg.appen("text")
-    .attr("class", "y-axis-title")
-    // more 
-    ;
-
-// create axes and axis title containers
-
 // (Later) Define update parameters: measure type, sorting direction
 
 // CHART UPDATE FUNCTION -------------------
-function update(data){
-	// update domains
-    xScale.domain(...)
-    yScale.domain(...)
+function update(data, type) {
+    const xScale = d3
+        .scaleBand()
+        .domain(d3.extent(data.map(d => d.company)))
+        .rangeRound([0, width])
+        .paddingInner(0.1);
 
+    const yScale = d3
+        .scaleLinear()
+        .domain(d3.extent(data.map(d => d[type])))
+        .range([height, 0]);
+
+    // create axes and axis title containers
+    svg.append('g')
+        .attr('class', 'axis y-axis')
+        .call(d3.axisLeft(yScale));
+    svg.append('g')
+        .attr('class', 'axis x-axis')
+        .attr("transform", "translate(0," + height + ")")
+        .call(d3.axisBottom(xScale).ticks(5, "s"));
+    svg.append("text")
+        .attr("class", "y-axis-title")
+        .text(type);
+
+    data.sort();
+    sortedLargestFirst = true;
+    document.getElementById('button').onclick = function() {
+        data.reverse();
+    };
+	// update domains
+    // xScale.domain(data.map(d => d.company));
+    // yScale.domain(data.map(d => d.type));
+
+    // update bars
     const bars = svg.selectAll('.bar')
         .data(data)
         .enter()
-        .append()
+        .append('rect')
+        .attr("x", function(d) { return xScale(d.Company); })
+        .attr("y", function(d) { return yScale(d[type]); })
         .attr("width", xScale.bandwidth())
-    
-	// update bars
+        .attr("height", function(d) { return height - yScale(d[type]); })
+        .attr("fill", "#69b3a2")
 
     // implement the enter-update-exist sequence
 
-	// update axes and axis title
+	// manually update axes and axis title
+    // d3.max(data, d => d[type])
+    // const xScale = d3
+    //     .scaleBand()
+    //     .domain(d3.extent(data.map(d => d.company)))
+    //     .rangeRound([0, width])
+    //     .paddingInner(0.1);
+    // const yScale = d3
+    //     .scaleLinear()
+    //     .domain(d3.extent(data.map(d => d[type])))
+    //     .range([height, 0]);
+
+    // change the measure dynamically:
+    //  d[type] where type is either "stores" or "revenue"
+    // svg.append('g')
+    //     .attr('class', 'axis y-axis')
+    //     .call(d3.axisLeft(yScale));
+    // svg.append('g')
+    //     .attr('class', 'axis x-axis')
+    //     .attr("transform", "translate(0," + height + ")")
+    //     .call(d3.axisBottom(xScale).ticks(5, "s"));
+    // svg.append("text")
+    //     .attr("class", "y-axis-title")
+    //     .text(type);
 
     // add a data key function (use company name as a key)
+
+    // svg.selectAll('.bar')
+    // .data(data)
+    // .attr('x', start)
+    // .merge()
+    // .transition()
+    // .duration(1000)
+    // .attr('x', end);
 }
 
 // CHART UPDATES ---------------------------
